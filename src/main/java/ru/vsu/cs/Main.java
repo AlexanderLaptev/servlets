e2423e7d3f84a9cc57c58f83653f6bdc110dfe32
Alexander Laptev <alexander_laptev4@inbox.ru>
2025-05-07 09:48:15 +0300

    run embedded Tomcat in Application.run()

--- src/main/java/ru/vsu/cs/Main.java
+++ src/main/java/ru/vsu/cs/Main.java
@@ -2,10 +2,13 @@ package ru.vsu.cs;
 
 import ru.vsu.cs.category.CategoryController;
 import ru.vsu.cs.framework.application.Application;
+import ru.vsu.cs.framework.application.configuration.ClasspathPropertiesConfiguration;
 
 public class Main {
     public static void main(String[] args) {
+        var config = new ClasspathPropertiesConfiguration("config.properties");
         var application = new Application.Builder()
+                .withConfiguration(config)
                 .addController(new CategoryController())
                 .build();
         application.run(args);
--- src/main/java/ru/vsu/cs/framework/application/Application.java
+++ src/main/java/ru/vsu/cs/framework/application/Application.java
@@ -1,18 +1,26 @@
 package ru.vsu.cs.framework.application;
 
+import org.apache.catalina.LifecycleException;
 import org.apache.catalina.startup.Tomcat;
+import ru.vsu.cs.framework.application.configuration.Configuration;
 import ru.vsu.cs.framework.controller.Controller;
 
 public class Application {
-    private Application() {
+    private final Configuration configuration;
+
+    private Application(Configuration configuration) {
+        this.configuration = configuration;
     }
 
     public static class Builder {
+        private Configuration configuration;
+
         public Application build() {
-            return new Application();
+            return new Application(configuration);
         }
 
-        public Builder setConfigPath(String configPath) {
+        public Builder withConfiguration(Configuration configuration) {
+            this.configuration = configuration;
             return this;
         }
 
@@ -23,5 +31,13 @@ public class Application {
 
     public void run(String[] commandLineArgs) {
         var tomcat = new Tomcat();
+        var serverPort = configuration.getIntProperty("server.port", 8080);
+        tomcat.setPort(serverPort);
+
+        try {
+            tomcat.start();
+        } catch (LifecycleException e) {
+            throw new RuntimeException(e);
+        }
     }
 }

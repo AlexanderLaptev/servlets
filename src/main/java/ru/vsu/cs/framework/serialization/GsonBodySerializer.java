a0544d4f35315b83091649cb6bd8c3fdf9f7a553
Alexander Laptev <alexander_laptev4@inbox.ru>
2025-05-07 14:43:53 +0300

    add date and time type adapters for GsonBodySerializer

--- src/main/java/ru/vsu/cs/framework/serialization/GsonBodySerializer.java
+++ src/main/java/ru/vsu/cs/framework/serialization/GsonBodySerializer.java
@@ -2,8 +2,52 @@ package ru.vsu.cs.framework.serialization;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
 
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+class LocalDateAdapter extends TypeAdapter<LocalDate> {
+    @Override
+    public void write(JsonWriter out, LocalDate value) throws IOException {
+        out.value(value.format(DateTimeFormatter.ISO_LOCAL_DATE));
+    }
+
+    @Override
+    public LocalDate read(JsonReader in) throws IOException {
+        return LocalDate.parse(in.nextString(), DateTimeFormatter.ISO_LOCAL_DATE);
+    }
+}
+
+class LocalTimeAdapter extends TypeAdapter<LocalTime> {
+    @Override
+    public void write(JsonWriter out, LocalTime value) throws IOException {
+        out.value(value.format(DateTimeFormatter.ISO_LOCAL_TIME));
+    }
+
+    @Override
+    public LocalTime read(JsonReader in) throws IOException {
+        return LocalTime.parse(in.nextString(), DateTimeFormatter.ISO_LOCAL_TIME);
+    }
+}
+
+class LocalDateTimeAdapter extends TypeAdapter<LocalDateTime> {
+    @Override
+    public void write(JsonWriter out, LocalDateTime value) throws IOException {
+        out.value(value.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
+    }
+
+    @Override
+    public LocalDateTime read(JsonReader in) throws IOException {
+        return LocalDateTime.parse(in.nextString(), DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+    }
+}
 
 public class GsonBodySerializer implements BodySerializer {
     public static final GsonBodySerializer INSTANCE = new GsonBodySerializer();
@@ -11,6 +55,9 @@ public class GsonBodySerializer implements BodySerializer {
     private GsonBodySerializer() { }
 
     private static final Gson GSON = new GsonBuilder()
+            .registerTypeAdapter(LocalDate.class, new LocalDateAdapter())
+            .registerTypeAdapter(LocalTime.class, new LocalTimeAdapter())
+            .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeAdapter())
             .create();
 
     @Override

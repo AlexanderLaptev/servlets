16a91e2638dec913edfd15e4f933eefad0729ca9
Alexander Laptev <alexander_laptev4@inbox.ru>
2025-05-07 09:25:09 +0300

    add Gson as a dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,4 +21,5 @@ dependencies {
     implementation("jakarta.servlet.jsp.jstl:jakarta.servlet.jsp.jstl-api:3.0.2")
     implementation("org.postgresql:postgresql:42.7.5")
     implementation("org.eclipse.jetty:apache-jstl:11.0.0")
+    implementation("com.google.code.gson:gson:2.13.1")
 }

2a6e0d0db3368165b7b4ddb633ca6f9f3cad54ac
Alexander Laptev <alexander_laptev4@inbox.ru>
2025-05-07 09:23:57 +0300

    migrate Gradle build scripts to Kotlin

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    java
+    war
+}
+
+group = "ru.vsu.cs.servlets"
+version = "0.0.1"
+
+tasks.withType<JavaCompile> {
+    sourceCompatibility = "21"
+    targetCompatibility = "21"
+    options.encoding = "UTF-8"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("jakarta.servlet:jakarta.servlet-api:6.1.0")
+    implementation("jakarta.servlet.jsp.jstl:jakarta.servlet.jsp.jstl-api:3.0.2")
+    implementation("org.postgresql:postgresql:42.7.5")
+    implementation("org.eclipse.jetty:apache-jstl:11.0.0")
+}
